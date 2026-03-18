35b90a221192e81535583a81ed1e404e4083ee53
Jalajk3004 <jalajkumar23989@gmail.com>
2026-03-18 02:09:32 +0000

    ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter, Outfit, Geist, Pacifico, Fredoka } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
 
 const inter = Inter({
   variable: "--font-inter",
